f89eaf724e9cedd0372ecfe61ef1282a4d848f23
daniela904 <87763778+daniela904@users.noreply.github.com>
2021-12-08 10:40:16 -0500

    Add files via upload

---
+++ AppReservas/views/__init__.py
@@ -0,0 +1,4 @@
+from .reserva_create_view   import ReservaCreateView;
+from .reserva_detail_view   import ReservaDetailView,AllReservasDetailView,AllUserReservasDetailView, ReservaUpdateView, ReservaDeleteView;
+from .verifyTokenView     import VerifyTokenView;
+from .plan_view            import  PlanDetailView,AllPlansDetailView,PlanForTimeDetailView,PlanForPriceDetailView, PlanUpdateView,  PlanDeleteView;
