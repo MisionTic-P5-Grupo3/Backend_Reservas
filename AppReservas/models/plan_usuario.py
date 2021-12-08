5384336fffa052ea23aae0e009e251bde4be7c64
daniela904 <87763778+daniela904@users.noreply.github.com>
2021-12-08 10:39:39 -0500

    Add files via upload

---
+++ AppReservas/models/plan_usuario.py
@@ -0,0 +1,12 @@
+from django.db    import models
+
+
+
+JORNADAS = (("Diurno","diurno"),("Nocturno","nocturno"))
+class Plan_usuario(models.Model): 
+    id_plan            = models.AutoField(primary_key=True)
+    jornada            = models.CharField('Jornada',choices=JORNADAS, max_length=10, default="Diurno")
+    nombre_plan        = models.CharField('Nombre del plan', max_length=50)
+    descripcion       = models.CharField('Nombre del plan', max_length=100)
+    precio             = models.FloatField('Precio', default= 0.00)
+    
