87852ca71606ca6a7d7b79f731dfb1f6dbe74adf
daniela904 <87763778+daniela904@users.noreply.github.com>
2021-12-15 00:01:40 -0500

    Update plan_serializer.py

--- AppReservas/serializers/plan_serializer.py
+++ AppReservas/serializers/plan_serializer.py
@@ -10,14 +10,3 @@ class PlanSerializer(serializers.ModelSerializer):
     class Meta:
         model = Plan_usuario
         fields =  ['id_plan','jornada','nombre_plan','descripcion','precio'] #campos obligatorios para la creacion del objeto
-
-    def to_representation(self, obj):
-        planData = Plan_usuario.objects.get(id_plan=obj.id_plan)
-        return {
-            'id_plan' : planData.id_plan,
-            'nombre_plan' : planData.nombre_plan,
-            'jornada'     : planData.jornada,
-            'descripcion' : planData.descripcion,
-            'precio'      : planData.precio,
-            'url': planData.url
-        }
