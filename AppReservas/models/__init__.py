83b65ea48ec3dafbcbb2f8144c3338d1930c22e1
daniela904 <87763778+daniela904@users.noreply.github.com>
2021-12-14 19:22:15 -0500

    Delete AppReservas directory

--- AppReservas/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from AppReservas.models.plan_usuario import Plan_usuario;
-from AppReservas.models.reserva_usuario import Reserva
--- AppReservas/views/__init__.py
+++
@@ -1,4 +0,0 @@
-from .reserva_create_view   import ReservaCreateView;
-from .reserva_detail_view   import ReservaDetailView,AllReservasDetailView,AllUserReservasDetailView, ReservaUpdateView, ReservaDeleteView;
-from .verifyTokenView     import VerifyTokenView;
-from .plan_view            import  PlanDetailView,AllPlansDetailView,PlanForTimeDetailView,PlanForPriceDetailView, PlanUpdateView,  PlanDeleteView;
